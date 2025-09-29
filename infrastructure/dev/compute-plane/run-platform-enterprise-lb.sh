30df322385caa6871812fff96dffee18d94a8ab9
Mükremin Çetinkaya <79249386+mkcetinkaya@users.noreply.github.com>
2025-09-29 13:35:41 +0300

    Update run-platform-enterprise-lb.sh

--- infrastructure/dev/compute-plane/run-platform-enterprise-lb.sh
+++ infrastructure/dev/compute-plane/run-platform-enterprise-lb.sh
@@ -261,6 +261,13 @@ set_up_nvidia_container_runtime () {
     apt-get update;
     apt-get install -y gnupg linux-headers-$(uname -r);
     apt-get install -y --no-install-recommends nvidia-driver-$NVIDIA_DRIVER_VERSION;
+	distribution=$(. /etc/os-release; echo $ID$VERSION_ID | sed -e 's/\.//g')
+    curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-docker-keyring.gpg
+    curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list |     sed 's/jammy/noble/' | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+    curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+    curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list |   sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' |   sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+    sudo apt-get update
+    sudo apt install nvidia-container-runtime -y
     sleep 2
     driver_pkg=$(dpkg -l | grep -i '^ii' | grep -E 'nvidia-driver-[0-9]+' | awk '{print $2}')
     if [ -n "$driver_pkg" ]; then
@@ -976,7 +983,7 @@ migManager:
     name: custom-mig-config
     default: ""
 toolkit:
-  enabled: true
+  enabled: false
 EOF
 
   elif [[ "$ts_used" == true ]]; then
@@ -992,7 +999,7 @@ devicePlugin:
 migManager:
   enabled: false
 toolkit:
-  enabled: true
+  enabled: false
 EOF
 
   elif [[ "$mig_used" == true ]]; then
@@ -1008,7 +1015,7 @@ migManager:
     name: custom-mig-config
     default: ""
 toolkit:
-  enabled: true
+  enabled: false
 EOF
 
   else
@@ -1020,7 +1027,7 @@ devicePlugin:
 migManager:
   enabled: false
 toolkit:
-  enabled: true
+  enabled: false
 EOF
   fi
 
