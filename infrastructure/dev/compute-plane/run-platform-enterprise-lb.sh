6333da046b4770308a9756bd4b99b0a2e277fc44
Mükremin Çetinkaya <79249386+mkcetinkaya@users.noreply.github.com>
2025-09-29 13:04:00 +0300

    Update run-platform-enterprise-lb.sh

--- infrastructure/dev/compute-plane/run-platform-enterprise-lb.sh
+++ infrastructure/dev/compute-plane/run-platform-enterprise-lb.sh
@@ -264,8 +264,7 @@ set_up_nvidia_container_runtime () {
     distribution=$(. /etc/os-release; echo $ID$VERSION_ID | sed -e 's/\.//g')
     curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-docker-keyring.gpg
     curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list |     sed 's/jammy/noble/' | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-    curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
-    curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list |   sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' |   sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+    curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list 
     sudo apt-get update
     sudo apt install nvidia-container-runtime -y
     sleep 2
