b502eb4ec23908a9e003541cb75ca2f0d17a707f
Mükremin Çetinkaya <79249386+mkcetinkaya@users.noreply.github.com>
2025-10-19 16:02:23 +0300

    Update run-platform-enterprise-lb.sh

--- infrastructure/dev/compute-plane/run-platform-enterprise-lb.sh
+++ infrastructure/dev/compute-plane/run-platform-enterprise-lb.sh
@@ -1058,15 +1058,13 @@ prometheus:
     enabled: true
     ingressClassName: nginx
     annotations:
-      nginx.ingress.kubernetes.io/rewrite-target: /$2
-      nginx.ingress.kubernetes.io/use-regex: "true"
       nginx.ingress.kubernetes.io/ssl-redirect: "true"
       nginx.ingress.kubernetes.io/backend-protocol: "HTTP"
     hosts:
       - ${SERVER_URL}
     paths:
-      - /prometheus(/|$)(.*)
-    pathType: ImplementationSpecific
+      - /prometheus
+    pathType: Prefix
     tls:
       - secretName: prod-tls
         hosts:
@@ -1077,7 +1075,7 @@ prometheus:
 
   prometheusSpec:
     externalUrl: https://${SERVER_URL}/prometheus/
-	routePrefix: /prometheus
+    routePrefix: /prometheus
     serviceMonitorSelectorNilUsesHelmValues: false
     podMonitorSelectorNilUsesHelmValues: false
     ruleSelectorNilUsesHelmValues: false
