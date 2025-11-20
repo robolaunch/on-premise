ac55090dda044d82e2ec46b4fd6f6acdecaa3361
Mükremin Çetinkaya <79249386+mkcetinkaya@users.noreply.github.com>
2025-11-20 10:21:28 +0300

    Update run-platform-script-enterprise.sh

--- infrastructure/dev/compute-plane/run-platform-script-enterprise.sh
+++ infrastructure/dev/compute-plane/run-platform-script-enterprise.sh
@@ -638,7 +638,8 @@ config:
     cookie_expire = '12h'
     redirect_url= 'https://$SERVER_URL/oauth2/callback'
     ssl_insecure_skip_verify = true
-    allowed_groups= '$GROUP'" > $DIR_PATH/oauth2-proxy/values.yaml;
+    allowed_groups = ['${GROUP}', '.*_super_admin']
+	oidc_groups_claim = 'groups'" > $DIR_PATH/oauth2-proxy/values.yaml;
 	    helm repo add oauth2-proxy https://oauth2-proxy.github.io/manifests
 		
         helm upgrade --install \
